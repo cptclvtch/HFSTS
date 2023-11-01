ffe8a2636caff93103ce0664ea555d2e8b88b522
cptclvtch <81629275+cptclvtch@users.noreply.github.com>
2023-11-01 07:40:15 -0600

    Switched to non-git crossplatform_app

--- general_purpose_graph/api.c
+++ general_purpose_graph/api.c
@@ -7,11 +7,18 @@ node_component* allocate_new_node_component()
 
     new_component = (node_component*)malloc(sizeof(node_component));
 
+    new_component.texts = (char**)malloc(sizeof(char*));
+    new_component.integers = (uint32_t*)malloc(sizeof(uint32_t));
+    new_component.floats = (float*)malloc(sizeof(float)); 
+
     return new_component;
 }
 
 void deallocate_node_component(node_component* target)
 {
+    free(target.floats);
+    free(target.integers);
+    free(target.texts);
     free(target);
 }
 
